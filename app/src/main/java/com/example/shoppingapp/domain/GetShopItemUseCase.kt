c1aa8e32267eb76af4d3522cfdba438d80e7048d
AldarBk <aldarrazer20@yandex.ru>
2023-03-15 10:45:07 +0600

    Work start

---
+++ app/src/main/java/com/example/shoppingapp/domain/GetShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppingapp.domain
+
+class GetShopItemUseCase {
+
+    fun getSHotItem(shopItemId: Int): ShopItem {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppingapp.domain
+
+class GetShopListUseCase {
+
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
